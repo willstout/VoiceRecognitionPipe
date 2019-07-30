d8c773319a43d38924f606077da0946ffccf3c1a
willstout <35538201+willstout@users.noreply.github.com>
2019-07-30 14:36:12 -0500

    Update read.py

--- read.py
+++ read.py
@@ -2,28 +2,53 @@ import subprocess
 import os
 import signal
 import requests
+import time
 
 APIRequestCount = 0
+writeMode = False
 p = subprocess.Popen("pocketsphinx_continuous -inmic yes", stdout=subprocess.PIPE, bufsize=1, shell=True)
+
 for line in iter(p.stdout.readline, b''):
     os.kill(p.pid, signal.SIGTSTP)
     line = str(line.lower())
     line = line[0:len(line)-1]
-    if "weather" in line or "whether" in line or "aether" in line or "whethers" in line:
-        response = requests.get('http://api.openweathermap.org/data/2.5/weather?zip=37240,us&APPID=10b4269b28392a82dff1ba86e8bc2717').text
-        tempStartIndex = response.find("temp")
-        kelvin = int(response[tempStartIndex+6:tempStartIndex+9])
-        fahrenheit = (kelvin - 273.15) * 1.8 + 32
-        print("The temperate is " + str(fahrenheit) + " degrees fahrenheit")
-        APIRequestCount += 1
-    elif "request" in line or "food" in line or "menu" in line:
-        print("Today there is Creamed Corn and more Creamed Corn")
-        APIRequestCount += 1
+    if writeMode:
+        if "close" in line:
+            APIRequestCount += 1
+            writeMode = False
+        else:
+            f = open("patient_info.txt","w+")
+            f.write(line)
+            f.close() 
     else:
-        print("I'm sorry, I heard " + line)
-        APIRequestCount += 1
+        if "patient" in line and "documentation" in line:
+            os.system("open patient_docs.jpg")
+            APIRequestCount += 1
+            writeMode = False
+        elif "write" in line:
+            APIRequestCount += 1
+            writeMode = True
+        elif "name" in line and "nurse" in line:
+            print("The name of your nurse is Jane Doe")
+            APIRequestCount += 1
+            writeMode = False
+        elif "weather" in line or "whether" in line:
+            response = requests.get('http://api.openweathermap.org/data/2.5/weather?zip=37240,us&APPID=10b4269b28392a82dff1ba86e8bc2717').text
+            tempStartIndex = response.find("temp")
+            kelvin = int(response[tempStartIndex+6:tempStartIndex+9])
+            fahrenheit = (kelvin - 273.15) * 1.8 + 32
+            print("The temperate is " + str(fahrenheit) + " degrees fahrenheit")
+            APIRequestCount += 1
+            writeMode = False
+        elif "timer" in line:
+            time.sleep(3)
+            print("Timer done")
+            APIRequestCount += 1
+            writeMode = False
+        else:
+            print("Sorry I didn't quite catch that")
 
-    if APIRequestCount < 2:
+    if APIRequestCount < 1:
         os.kill(p.pid, signal.SIGCONT)
     else:
         os.kill(p.pid, signal.SIGKILL)
